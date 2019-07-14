35c0ce2c887dba12f3c89d7414a31a34f6f419b9
Nancy <chemutain@gmail.com>
2019-07-14 22:25:15 +0300

    updates

--- app/src/main/java/com/example/phaseonechallengeandroid/ActivityB.java
+++ app/src/main/java/com/example/phaseonechallengeandroid/ActivityB.java
@@ -2,10 +2,18 @@ package com.example.phaseonechallengeandroid;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.net.Uri;
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,23 +22,35 @@ import butterknife.OnClick;
 public class ActivityB extends AppCompatActivity {
 
     @BindView(R.id.webview_about)
-    WebView webview_about;
+       WebView webView_about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
         ButterKnife.bind(this);
-//        webView_about = findViewById(R.id.webview_about);
-//        webView();
+//        String url = "https://andela.com/alc/";
+        WebSettings ws = webView_about.getSettings();
+        ws.setJavaScriptEnabled(true);//enable javascript in my web app
+        ws.setUseWideViewPort(true);
+        ws.setLoadWithOverviewMode(true);
+        webView_about.setWebViewClient(new WebViewClient());//prevents url from opening from browser
+        webView_about.loadUrl("https://andela.com/alc/");
+
     }
+    private class WebClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
 
-//    public void webView(){
-//        webView_about.loadUrl("https://andela.com/alc/");
-//    }
-
-    @OnClick(R.id.webview_about)
-    public void aboutWebView(){
-        webview_about.loadUrl("https://andela.com/alc/");
+        String TAG = "Error";
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+            Log.d(TAG, "onReceivedSslError: "+error);
+            Toast.makeText(ActivityB.this, "Error" + handler, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/phaseonechallengeandroid/WebClient.java
@@ -0,0 +1,21 @@
+package com.example.phaseonechallengeandroid;
+
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebClient extends WebViewClient {
+//    String url = "https://andela.com/alc/";
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed();
+    }
+}
