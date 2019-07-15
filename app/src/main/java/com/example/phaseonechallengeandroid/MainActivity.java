478ac577a9c36bac9e9a4d5f6767990def2a7099
Nancy <chemutain@gmail.com>
2019-07-15 10:35:53 +0300

    updates

--- app/src/main/java/com/example/phaseonechallengeandroid/MainActivity.java
+++ app/src/main/java/com/example/phaseonechallengeandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.phaseonechallengeandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
@@ -32,4 +33,5 @@ public class MainActivity extends AppCompatActivity {
     public void btnTwo(){
         startActivity(new Intent(MainActivity.this, ActivityC.class));
     }
+
 }
